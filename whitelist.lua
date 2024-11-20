21ae2a4afeee3ff2b8b78844ec40cef7c8aa9ac6
Dev <125480854+scriptmaker187@users.noreply.github.com>
2024-11-20 18:48:55 +0100

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -4,9 +4,11 @@ return {
     [3185160074] = true,  -- ~~ 17
     [5178800079] = true,  -- ~~ 32
     [1607402326] = true,  -- ~~ 28
-    [7209798301] = true,  -- Jungergottxalt2
-    [4981997317] = true,  -- Jungergottalt2
+    [7209798301] = false,  -- Jungergottxalt2
+    [4981997317] = false,  -- Jungergottalt2
     [2297130039] = false, -- Dub
-    [2757933792] = true,  -- Jungergottalt1
+    [2757933792] = false,  -- Jungergottalt1
     [7218080978] = true,  -- Frite
+    [6113226774] = true,  -- Teddy alt4
+    [7217204442] = true,  -- FLorian alt3
 }
