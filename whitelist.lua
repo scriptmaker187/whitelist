5884648d6b3c8adcba23d238e171f97ea7cf156d
Dev <125480854+scriptmaker187@users.noreply.github.com>
2025-02-06 02:03:42 +0100

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -6,7 +6,7 @@ return {
     [1607402326] = true,  -- ~~ 28
     [7209798301] = false,  -- Jungergottxalt2
     [4981997317] = false,  -- Jungergottalt2
-    [2297130039] = false, -- Dub
+    [2297130039] = true, -- Dub
     [2757933792] = false,  -- Jungergottalt1
     [7218080978] = true,  -- Frite
     [6113226774] = false,  -- Teddy alt4
